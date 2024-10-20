623cc85ad439ac2785f30e0cc12630a6f3f8e89a
noorhonjol <mayaabdoh2002@gmail.com>
2024-10-20 21:49:58 +0300

    feat(temperature): add temperature API

--- app.py
+++ app.py
@@ -18,3 +18,27 @@ def version() -> str:
     """
     return VERSION
 
+@app.get("/temperature")
+def temperature() -> float:
+    """
+    Returns the average temperature in celcius
+    across all opensensemap boxes
+    """
+    phenomenon = "temperature"
+    date = datetime.now().isoformat() + 'Z'
+    res = requests.get(
+            f"https://api.opensensemap.org/boxes?date={date}&phenomenon={phenomenon}",
+            timeout=1000
+    )
+    all_boxes = res.json()
+    temperatures = [
+        float(sensor['lastMeasurement']['value'])
+        for box in all_boxes
+        for sensor in box['sensors']
+        if sensor.get('unit') == '°C' and 'lastMeasurement' in sensor
+    ]
+
+    if not temperatures:
+        return 0
+
+    return sum(temperatures) / len(temperatures)
--- test_app.py
+++ test_app.py
@@ -15,3 +15,28 @@ def test_version():
     assert response.status_code == 200
     assert response.json() == "v0.0.1"
 
+@patch('requests.get')
+def test_temperature(mock_get):
+    """
+    Test the /temperature endpoint by mocking the requests.get call
+    """
+    # Mock response data
+    mock_response_data = [
+        {
+            'sensors': [
+                {'unit': '°C', 'lastMeasurement': {'value': '22.5'}},
+                {'unit': '°C', 'lastMeasurement': {'value': '24.0'}},
+            ]
+        },
+        {
+            'sensors': [
+                {'unit': '°C', 'lastMeasurement': {'value': '21.5'}}
+            ]
+        }
+    ]
+
+    mock_get.return_value.json.return_value = mock_response_data
+
+    response = client.get("/temperature")
+    assert response.status_code == 200
+    assert response.json() == (22.5+24.0+21.5)/3  # Average of 22.5, 24.0, and 21.5
